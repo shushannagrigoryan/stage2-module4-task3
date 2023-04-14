12282baedad1f0ac73e4a8df62bfbc7bb751ab70
Shushanna Grigoryan <shush.grig.github@gmail.com>
2023-04-14 21:14:38 +0400

    2

--- task1-strategy-pattern/src/main/java/com/mjc/stage2/impl/ManufactureFilteringStrategy.java
+++ task1-strategy-pattern/src/main/java/com/mjc/stage2/impl/ManufactureFilteringStrategy.java
@@ -1,11 +1,14 @@
 package com.mjc.stage2.impl;
 
 
+import com.mjc.stage2.FilteringStrategy;
 import com.mjc.stage2.Product;
 import lombok.AllArgsConstructor;
 
+import java.io.FileFilter;
+
 @AllArgsConstructor
-public class ManufactureFilteringStrategy {
+public class ManufactureFilteringStrategy implements FilteringStrategy {
     private String manufacture;
 
     // Write your code here!
